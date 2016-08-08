9e32fccaf55824fdc7471f826345c9db3fed373a
Rahul Kumar <rahulcomp24@gmail.com>
2016-08-08 12:50:30 +0530

    Move Relative Directory to Demo

---
+++ Demo/RelativeDirectory/Product/myScript.js
@@ -0,0 +1,6 @@
+var myElement = document.createElement("p");
+var node = document.createTextNode("This is loaded using myScript.js");
+myElement.appendChild(node);
+
+var myParaDiv = document.getElementById("myParas");
+myParaDiv.appendChild(myElement);
